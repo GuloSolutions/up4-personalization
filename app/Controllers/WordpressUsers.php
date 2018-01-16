bd0d926861ebb18f505a0226989d288a6838d904
zmonteca <zwilson@umich.edu>
2018-01-16 11:59:45 -0600

    fixed: changed constructor to accept min number of params to create wp user

--- app/Controllers/WordpressUsers.php
+++ app/Controllers/WordpressUsers.php
@@ -3,46 +3,59 @@ namespace Controllers;
 
 use Illuminate\Database\Eloquent\Model as Model;
 use Models\User;
+use Carbon\Carbon;
 
-class WordpressUsers {
+class WordpressUsers
+{
+    private $data;
 
-    public function __construct() {}
+    public function __construct($name, $email = null)
+    {
+        $this->email = !empty($email) ? $email : Facebook_Social_Public::emailGenerator();
+        $this->nice_name = preg_replace('/\s/', '-', $name);
 
-    public function setupWPUser (Array $data)
+        $this->setData();
+    }
+
+    private function setData()
+    {
+        $plain_text = wp_generate_password(20);
+        $user_pass = wp_hash_password($plain_text);
+
+        $names = explode('-', $this->nice_name);
+
+        $this->data = [
+            'user_login' => $this->email,
+            'user_pass' => $user_pass,
+            'user_email' => $this->email,
+            'display_name' => sprintf('%s %s', $names[0], $names[1]),
+            'user_nicename' => $this->nice_name,
+            'user_url' => '',
+            'user_registered' => Carbon::now(),
+            'user_activation_key' => '',
+            'user_status' => 0
+        ];
+    }
+
+    public function setup()
     {
         // see if we already have a user
-        $user = User::where('user_email', $data['user_email'])
+        $user = User::where('user_email', $this->email)
             ->first();
 
         // add the WordPress users
         if ($user === null) {
-            $user = $this->newWPUser($data);
+            $user = $this->create();
         }
 
         return $user;
     }
 
-    public function newWPUser (Array $data)
+    public function create()
     {
+        $user = new User($this->data);
+        $user->save();
 
-        $plain_text = wp_generate_password(20);
-        $user_pass = wp_hash_password($plain_text);
-
-        $user_data = [
-                'user_login' => $data['user_email'],
-                'user_pass' => $user_pass,
-                'user_email' => $data ['user_email'],
-                'display_name' => sprintf('%s %s', $data['first_name'], $data['last_name']),
-                'user_nicename' => $data['user_nicename'],
-                'user_url' => '',
-                'user_registered' => Carbon::now(),
-                'user_activation_key' => '',
-                'user_status' => 0
-        ];
-
-        $wp_user = new User($user_data);
-        $wp_user->save();
-
-        return $wp_user;
+        return $user;
     }
 }
