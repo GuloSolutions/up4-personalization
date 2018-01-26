25fe7eb020edf0160900c6cad03cf188c6a6ca19
radboris <radboris@goldmember.gulosolutions.com>
2018-01-26 15:29:39 -0600

    insert survey after head tag

--- public/js/showSurvey-social-public.js
+++ public/js/showSurvey-social-public.js
@@ -1,11 +1,14 @@
 $(function() {
     $.ajax({
-    url: ajax_receiver.ajax_url,
-}).success(function (e) {
-    $('#show-survey').on('click', function(e) {
+        url: ajax_receiver.ajax_url,
+        }).success(function (e) {
+        $('#show-survey').on('click', function(e) {
 
-    $('#survey-social-public').show();
+        $('#survey-social-public').insertAfter($('head'));
+
+        $('#survey-social-public').show();
         });
     })
+
 });
 
