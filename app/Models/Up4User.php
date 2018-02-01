04f2b05bb19fcb2ea57f445e5323a6334fc9d05c
radboris <radboris@goldmember.gulosolutions.com>
2018-02-01 12:25:42 -0600

    update Up4User model with survey checkbox

--- app/Models/Up4User.php
+++ app/Models/Up4User.php
@@ -11,6 +11,7 @@ class Up4User extends Model
         'age', 'first_name', 'last_name','session_id',
         'facebook_id', 'gender', 'zip',
         'has_children', 'travels_often', 'exercises_often',
+        'digestive', 'immune', 'vaginal', 'urinary',
         'weather', 'local_time', 'picture'
     ];
 
