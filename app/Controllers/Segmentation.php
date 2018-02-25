9294cd5a511215f11e302cd4169dc87eec3dcea2
radboris <radboris@goldmember.gulosolutions.com>
2018-02-24 20:51:09 -0600

    Segmentation class2

--- app/Controllers/Segmentation.php
+++ app/Controllers/Segmentation.php
@@ -52,8 +52,6 @@ class Segmentation
      */
     public function segmentedUserTemplate()
     {
-        // global $up4_user;
-
         if (!$up4_user->isLoggedIn() && $this->isCookieSet()) {
             return $this->user_segment;
         }
@@ -67,27 +65,25 @@ class Segmentation
         global $up4_user;
         global $wp_query;
 
-        // if (!$up4_user->isLoggedIn() && array_key_exists('utm_content', $_GET)) {
-        //     $segments = explode('_', $_GET['utm_content']);
+        if (!$up4_user->isLoggedIn() && array_key_exists('utm_content', $_GET)) {
+            $segments = explode('_', $_GET['utm_content']);
 
-        //     $segment = strtolower($segments[0]);
+            $segment = strtolower($segments[0]);
 
-        $segment = strtolower('Ultra');
+            // get our custom segment
+            $wp_query_clone = clone $wp_query;
+            $wp_query = new \WP_Query(['title' => $segment, 'post_type' => 'segments']);
 
-        // get our custom segment
-        $wp_query_clone = clone $wp_query;
-        $wp_query = new \WP_Query(['title' => $segment, 'post_type' => 'segments']);
+            if ($wp_query->have_posts() && $this->isCookieSet()) {
+                setup_postdata($wp_query->the_post());
 
-        if ($wp_query->have_posts() && $this->isCookieSet()) {
-            setup_postdata($wp_query->the_post());
-
-            if (!empty(get_template_part('single-segments'))) {
-                $this->user_segment = get_template_part('single-segments');
-                return true;
-            } else {
-                return false;
+                if (!empty(get_template_part('single-segments'))) {
+                    $this->user_segment = get_template_part('single-segments');
+                    return true;
+                } else {
+                    return false;
+                }
             }
         }
     }
 }
-//}
