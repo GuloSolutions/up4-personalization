2e60abb3f5a10362502f88b4ea3b00f0d535f463
radboris <radboris@goldmember.gulosolutions.com>
2018-03-01 12:01:45 -0600

    update Coupon.php for encoding offer codes

--- app/Controllers/Coupon.php
+++ app/Controllers/Coupon.php
@@ -7,11 +7,14 @@ use Models\Up4User;
 
 class Coupon
 {
-
     //provided by Coupons.com
     private $checkCode;
+    //dummy values for now
+    const PIN_CODE='wdoidjqoixmxq[]qd-02-';
+    const SHORT_KEY= 'DJHDWQE233203289HDD0D29-';
+    const LONG_KEY = 'DSDSKLDSHLDAJKDAH';
 
-    public function encodeRequest($pinCode, $offerCode, $shortKey, $longKey)
+    public function encodeRequest($offerCode)
     {
         $decodeX = " abcdefghijklmnopqrstuvwxyz0123456789!$%()*+,-.@;<=>?[]^_{|}~";
         $encodeModulo = array_fill(0, 256, 0);
@@ -21,7 +24,7 @@ class Coupon
         for ($i = 0; $i < 61; $i++) {
             $encodeModulo[substr($decodeX, $i, 1)] = $i;
         }
-        $pinCode = strtolower($pinCode) . strval($offerCode);
+        $pinCode = strtolower(self::PIN_CODE) . strval($offerCode);
         if (strlen($pinCode) < 20) {
             $pinCode .= ' couponsincproduction';
             $pinCode = substr($pinCode, 0, 20);
@@ -29,16 +32,16 @@ class Coupon
 
         $q = 0;
         $j = strlen($pinCode);
-        $k = strlen($shortKey);
+        $k = strlen(self::SHORT_KEY);
         $s1 = $s2 = $s3 = null;
         $cpt = '';
 
         for ($i = 0; $i < $j; $i++) {
             $s1 = $encodeModulo[substr($pinCode, $i, 1)];
-            $s2 = 2 * $encodeModulo[substr($shortKey, $i % $k, 1)];
+            $s2 = 2 * $encodeModulo[substr(self::SHORT_KEY, $i % $k, 1)];
             $s3 = $vob[$i % 2];
             $q = ($q + $s1 + $s2 + $s3) % 61;
-            $cpt .= substr($longKey, $q, 1);
+            $cpt .= substr(self::LONG_KEY, $q, 1);
         }
         return ['o' => $offerCode, 'cpt' =>$cpt];
     }
