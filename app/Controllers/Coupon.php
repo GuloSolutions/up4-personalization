625621a6ede279a76cda230e9af7b25fae10c77b
radboris <radboris@goldmember.gulosolutions.com>
2018-03-08 10:11:38 -0600

    updated Coupon.php to allow for setting props

--- app/Controllers/Coupon.php
+++ app/Controllers/Coupon.php
@@ -11,23 +11,34 @@ class Coupon
 
     const DEVELOPEMENT_CPT_OVERIDE = 'ndq2QwGjFT97N6h6lyRD4fVT9wL';
 
-    private $checkCode;
+    private $short_cipher;
+    private $long_cipher;
+    private $offer_code;
+
+
+    public function __construct(string $long_cipher, string $short_cipher, string $offer_code)
+    {
+        $this->setLongCipher($long_cipher);
+        $this->setShortCipher($short_cipher);
+        $this->setOfferCode($offer_code);
+    }
+
     // SHORT_KEY and LONG_KEY are production keys;
     const PIN_CODE='wdoidjqoixmxq[]qd-02-';
     const SHORT_KEY= 'vcb4fdqpzw';
     const LONG_KEY = 'ZtQ7OsPjRc1XAex8DL3ulMYig65naf9zNyFBv2oTpwGkVWShUIKqEC4JHdbrm';
 
-    public function encodeRequest($offerCode)
+    public function encodeRequest()
     {
         $decodeX = " abcdefghijklmnopqrstuvwxyz0123456789!$%()*+,-.@;<=>?[]^_{|}~";
         $encodeModulo = array_fill(0, 256, 0);
-        $vob[0] = $offerCode % 100;
-        $vob[1] = ($offerCode / 100) % 100;
+        $vob[0] = $this->offer_code % 100;
+        $vob[1] = ($this->offer_code / 100) % 100;
 
         for ($i = 0; $i < 61; $i++) {
             $encodeModulo[substr($decodeX, $i, 1)] = $i;
         }
-        $pinCode = strtolower(self::PIN_CODE) . strval($offerCode);
+        $pinCode = strtolower(self::PIN_CODE) . strval($this->offer_code);
         if (strlen($pinCode) < 20) {
             $pinCode .= ' couponsincproduction';
             $pinCode = substr($pinCode, 0, 20);
@@ -52,9 +63,25 @@ class Coupon
         }
 
         return [
-            'o' => $offerCode,
+            'o' => $this->offer_code,
             'p' => self::PCODE,
             'cpt' =>$cpt
         ];
     }
+
+    public function setLongCipher(string $long_cipher)
+    {
+        $this->long_cipher = $long_cipher;
+    }
+
+    public function setShortCipher(string $short_cipher)
+    {
+        $this->short_cipher = $short_cipher;
+    }
+
+    public function setOfferCode(string $offer_code)
+    {
+        $this->offer_code = $offer_code;
+    }
+
 }
