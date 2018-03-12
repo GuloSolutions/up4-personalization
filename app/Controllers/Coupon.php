da635fbeaec1a9ded484bad973c9df8c163e83bd
radboris <radboris@goldmember.gulosolutions.com>
2018-03-12 13:04:38 -0500

    update branch with Couppon.php

--- app/Controllers/Coupon.php
+++ app/Controllers/Coupon.php
@@ -9,26 +9,33 @@ class Coupon
 {
     const PCODE = 'camhong2296@yahoo.com';
 
-    const DEVELOPEMENT_CPT_OVERIDE = 'ndq2QwGjFT97N6h6lyRD4fVT9wL';
+    private $short_cipher;
+    private $long_cipher;
+    private $offer_code;
 
-    // provided by Coupons.com
-    private $checkCode;
-    // LONG_KEY and SHORT_CODE are production; PIN_CODE needs update
-    const PIN_CODE='wdoidjqoixmxq[]qd-02-';
-    const SHORT_KEY= 'DJHDWQE233203289HDD0D29';
-    const LONG_KEY = 'DSDSKLDSHLDAJKDAH';
 
-    public function encodeRequest($offerCode)
+    public function __construct(string $long_cipher, string $short_cipher, string $offer_code)
     {
+        $this->setLongCipher($long_cipher);
+        $this->setShortCipher($short_cipher);
+        $this->setOfferCode($offer_code);
+    }
+
+    public function encodeRequest()
+    {
+        if (!isset($this->long_cipher) || !isset($this->short_cipher) || !isset($this->offer_code)) {
+            return false;
+        }
+
         $decodeX = " abcdefghijklmnopqrstuvwxyz0123456789!$%()*+,-.@;<=>?[]^_{|}~";
         $encodeModulo = array_fill(0, 256, 0);
-        $vob[0] = $offerCode % 100;
-        $vob[1] = ($offerCode / 100) % 100;
+        $vob[0] = $this->offer_code % 100;
+        $vob[1] = ($this->offer_code / 100) % 100;
 
         for ($i = 0; $i < 61; $i++) {
             $encodeModulo[substr($decodeX, $i, 1)] = $i;
         }
-        $pinCode = strtolower(self::PIN_CODE) . strval($offerCode);
+        $pinCode = strtolower(self::PIN_CODE) . strval($this->offer_code);
         if (strlen($pinCode) < 20) {
             $pinCode .= ' couponsincproduction';
             $pinCode = substr($pinCode, 0, 20);
@@ -36,26 +43,37 @@ class Coupon
 
         $q = 0;
         $j = strlen($pinCode);
-        $k = strlen(self::SHORT_KEY);
+        $k = strlen($this->short_cipher);
         $s1 = $s2 = $s3 = null;
         $cpt = '';
 
         for ($i = 0; $i < $j; $i++) {
             $s1 = $encodeModulo[substr($pinCode, $i, 1)];
-            $s2 = 2 * $encodeModulo[substr(self::SHORT_KEY, $i % $k, 1)];
+            $s2 = 2 * $encodeModulo[substr($this->short_cipher, $i % $k, 1)];
             $s3 = $vob[$i % 2];
             $q = ($q + $s1 + $s2 + $s3) % 61;
-            $cpt .= substr(self::LONG_KEY, $q, 1);
-        }
-
-        if(self::DEVELOPEMENT_CPT_OVERIDE) {
-            $cpt = self::DEVELOPEMENT_CPT_OVERIDE;
+            $cpt .= substr($this->long_cipher, $q, 1);
         }
 
         return [
-            'o' => $offerCode,
+            'o' => $this->offer_code,
             'p' => self::PCODE,
             'cpt' =>$cpt
         ];
+    }
+
+    public function setLongCipher(string $long_cipher)
+    {
+        $this->long_cipher = $long_cipher;
+    }
+
+    public function setShortCipher(string $short_cipher)
+    {
+        $this->short_cipher = $short_cipher;
+    }
+
+    public function setOfferCode(string $offer_code)
+    {
+        $this->offer_code = $offer_code;
     }
 }
