788e7753f28a294e8db171c2a5f7598edaaa758c
radboris <radboris@goldmember.gulosolutions.com>
2018-03-22 19:37:11 -0500

    add hash for survey

--- public/js/showSurvey-social-public.js
+++ public/js/showSurvey-social-public.js
@@ -1,6 +1,19 @@
-$(function() {
+(function ($) {
+    $activate_survey = function() {
+        $('#survey-social-public').addClass('active');
+        $('.site-header').addClass('under');
+    };
+
     $('#show-survey, #show-survey-again').on('click', function(e) {
         $('#survey-social-public').addClass('active');
         $('.site-header').addClass('under');
     });
-});
+
+    $(window).on('hashchange', function() {
+        if (window.location.hash.slice(1) == "take-survey") {
+            $('#survey-social-public').addClass('active');
+            $('.site-header').addClass('under');
+        }
+    });
+
+})(jQuery);
