c4ab69ea78f0fd2b630a75827406c2531bb00645
zmonteca <zwilson@umich.edu>
2018-03-28 23:25:53 -0500

    fixed: more than 1 header fail would result in multiple lookups; code clean up

--- app/Controllers/IpAddress.php
+++ app/Controllers/IpAddress.php
@@ -23,7 +23,17 @@ class IpAddress
     {
         $ping_response = [];
 
-        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+        $response_headers = [
+            'HTTP_CLIENT_IP',
+            'HTTP_X_FORWARDED_FOR',
+            'HTTP_X_FORWARDED',
+            'HTTP_X_CLUSTER_CLIENT_IP',
+            'HTTP_FORWARDED_FOR',
+            'HTTP_FORWARDED',
+            'REMOTE_ADDR'
+        ];
+
+        foreach ($response_headers as $key) {
             if (array_key_exists($key, $_SERVER) === true) {
                 foreach (explode(',', $_SERVER[$key]) as $ip) {
                     // just to be safe
@@ -31,23 +41,22 @@ class IpAddress
 
                     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
                         $this->address = $ip;
-                    } else {
-                        $host = 'www.cnn.com';
-                        exec("nslookup $host", $result);
-
-                        //this loop will get the last ipaddress but this is what we want
-                        foreach ($result as $key => $value) {
-                            if (preg_match('/\d+(?:\.\d+)+/', $value, $matches)) {
-                                $ip = $matches[0];
-                            }
-                        }
-                        $ip = trim($ip);
-                        $this->address = $ip;
                     }
                 }
             }
         }
 
+        if (!$this->address) {
+            $this->address = $this->getFixedAddress();
+        }
+
         return false;
+    }
+
+    private function getFixedAddress()
+    {
+        $ip = gethostbyname('www.cnn.com');
+
+        return $ip;
     }
 }
