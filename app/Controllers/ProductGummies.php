c20e2c44cc50d981608670c301b19d47e312d45b
radboris <rad.borislavov@gmail.com>
2018-09-22 19:04:16 -0500

    update plugin and js

---
+++ app/Controllers/ProductGummies.php
@@ -0,0 +1,79 @@
+<?php
+namespace Controllers;
+
+use Controllers\Gender;
+use Controllers\Age;
+
+class ProductGummies extends AbstractProduct
+{
+    private const SKU = 40000610000;
+
+    private const RANK = 1;
+
+    public $gender;
+
+    public $age;
+
+    public function __construct()
+    {
+        $this->gender = new Gender(Gender::BOTH);
+        $this->age = new Age(Age::ISALL);
+
+        $this->rank = self::RANK;
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getSku()
+    {
+        return self::SKU;
+    }
+
+    public function isTravelsOften()
+    {
+        return false;
+    }
+
+    public function isExercisesOften()
+    {
+        return false;
+    }
+
+    public function hasChildren()
+    {
+        return false;
+    }
+
+    public function isUrinary()
+    {
+        return false;
+    }
+
+    public function isVaginal()
+    {
+        return false;
+    }
+
+    public function isDigestive()
+    {
+        return true;
+    }
+
+    public function isImmune()
+    {
+        return true;
+    }
+
+    public function isCapsules()
+    {
+        return false;
+    }
+}
